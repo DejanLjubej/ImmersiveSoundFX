a752fe632a52c5604dd83d67a965835d4bc16f52
HBGames <dejan.ljubej@gmail.com>
2019-09-05 14:31:59 +0200

    5.9. grid and button look

--- lib/Widgets/buttonState.dart
+++ lib/Widgets/buttonState.dart
@@ -19,15 +19,36 @@ class _ExampleAppState extends State<ButtonListDisplay> {
         appBar: AppBar(
           title: Text('Situational Sound Effects'),
         ),
-        body: Center(
-          child: ListView.builder(
-            itemCount: _number,
-            itemBuilder: (BuildContext context, int index){
-              return tryoutButton(index);
-            }
-          ),
-        ), 
+        body: 
+        GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(100, (index) {
+            return Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headline,
+              ),
+            );
+          }),
+        ),  
+        // Center(
+        //   child: ListView.builder(
+        //     itemCount: _number,
+        //     itemBuilder: (BuildContext context, int index){
+        //       return tryoutButton(index);
+        //     }
+        //   ),
+        // ), 
 
       );
   }
+  Widget buttonLayout(){
+    return Container(
+
+    );
+  }
+
 }
--- lib/Widgets/soundButton.dart
+++ lib/Widgets/soundButton.dart
@@ -2,22 +2,33 @@ import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-Widget tryoutButton(int i){
+Widget tryoutButton(int i) {
   AudioCache audioCache = new AudioCache();
-    return  FlatButton(
-
-      color: Colors.blue,
-      textColor: Colors.white,
-      disabledColor: Colors.grey,
-      disabledTextColor: Colors.black,
-      padding: EdgeInsets.all(8.0),
-      splashColor: Colors.blueAccent,
-      onPressed: () {
-        audioCache.play('running.wav');
-      },
-      child: Text(
-        i.toString(),
-        style: TextStyle(fontSize: 20.0),
-      ),
-    );
-  }
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          RaisedButton(
+            shape: CircleBorder(),
+            elevation: 10,
+            color: Colors.blue,
+            textColor: Colors.white,
+            disabledColor: Colors.grey,
+            disabledTextColor: Colors.black,
+            padding: EdgeInsets.all(50.0),
+            splashColor: Colors.blueAccent,
+            onPressed: () {
+              audioCache.play('running.wav');
+            },
+            child: Text(
+              i.toString(),
+              style: TextStyle(fontSize: 20.0),
+            ),
+          )
+        ],
+      )
+    ],
+  );
+}
