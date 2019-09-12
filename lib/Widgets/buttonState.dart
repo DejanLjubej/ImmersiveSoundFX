bfaa5f8604337d81f8018fbe020276ff200b1ca8
HBGames <dejan.ljubej@gmail.com>
2019-09-12 12:26:08 +0200

    coppied custom painter class

--- lib/Widgets/buttonState.dart
+++ lib/Widgets/buttonState.dart
@@ -1,6 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'buttonRow.dart';
 
+
+const kMinScrollBarHeight = 20.0;
 class ButtonListDisplay extends StatefulWidget {
   @override
   _ExampleAppState createState() => _ExampleAppState();
@@ -11,6 +15,7 @@ class _ExampleAppState extends State<ButtonListDisplay> {
   double _height = 150;
   int _i = -2;
   int _number = 50;
+  double _scrollBarOffset = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -82,6 +87,62 @@ class _ExampleAppState extends State<ButtonListDisplay> {
           ],
         ),
 
+      ),
+    );
+  }
+
+  //widget responsible for displaying the list of sounds
+  Widget buttonList() {
+    return ListView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: _number,
+        shrinkWrap: true,
+        itemBuilder: (BuildContext context, int index) {
+          _i += 2;
+          return rowOfButtons(_i);
+        });
+  }
+}
+
+class MyDraggable extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()..color = Colors.white;
+    final Radius cornerRadius = Radius.circular(20.0);
+    final double lineMargin = 30.0;
+
+    // Draw slider
+    canvas.drawRRect(
+        RRect.fromLTRBAndCorners(0.0, 0.0, size.width, size.height,
+            bottomLeft: cornerRadius, bottomRight: cornerRadius),
+        paint);
+    paint.color = Colors.black.withAlpha(64);
+    paint.strokeWidth = 1.5;
+
+    // Draw triangle
+    canvas.drawPoints(
+      
+        PointMode.polygon,
+        [
+          Offset((size.width / 2) - 5.0, size.height - 10.0),
+          Offset((size.width / 2) + 5.0, size.height - 10.0),
+          Offset((size.width / 2), size.height - 5.0),
+          Offset((size.width / 2) - 5.0, size.height - 10.0),
+        ],
+        paint);
+
+    // Draw line
+    canvas.drawLine(Offset(lineMargin, size.height - kMinScrollBarHeight),
+        Offset(size.width - lineMargin, size.height - kMinScrollBarHeight), paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+
+
+// comented code that might be usefull at some poin
         // Column(
         //   crossAxisAlignment: CrossAxisAlignment.center,
         //   mainAxisAlignment: MainAxisAlignment.center,
@@ -137,19 +198,3 @@ class _ExampleAppState extends State<ButtonListDisplay> {
         //         )),
         //   ],
         // ),
-      ),
-    );
-  }
-
-  //widget responsible for displaying the list of sounds
-  Widget buttonList() {
-    return ListView.builder(
-        scrollDirection: Axis.vertical,
-        itemCount: _number,
-        shrinkWrap: true,
-        itemBuilder: (BuildContext context, int index) {
-          _i += 2;
-          return rowOfButtons(_i);
-        });
-  }
-}
